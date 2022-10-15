dd1493c8f0883dde30ef9bee331d245b42530708
Brian W. Wolter <brian@brianwolter.com>
2022-10-15 11:17:33 -0400

    ...

--- src/rdl/scan.rs
+++ src/rdl/scan.rs
@@ -38,7 +38,7 @@ pub struct Token {
 
 impl fmt::Display for Token {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "<{:?} \"{}\">", self.ttype, self.ttext)
+    write!(f, "<{}>", self.ttext)
   }
 }
 
