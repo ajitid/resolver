d0dcac116ffb44430e5e9778a34c02eec0fca143
Brian W. Wolter <brian@brianwolter.com>
2022-10-22 23:36:27 -0400

    ...

--- src/text.rs
+++ src/text.rs
@@ -23,7 +23,7 @@ pub struct Line {
 
 impl Line {
   pub fn text<'a>(&self, text: &'a str) -> &'a str {
-    &text[self.offset..self.extent]
+    &text[self.offset..self.offset + self.bytes]
   }
   
   pub fn end(&self) -> usize {
